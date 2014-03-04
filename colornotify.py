8d2783ac89381adc6c22bf7cdab82fde1c9326cc
Alexander Lozovskoy <alozovskoy@gmail.com>
2014-03-04 23:47:10 +0400

    Скрипт для оповещения

---
+++ colornotify.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import imp
+
+colorModule = imp.load_source('', sys.argv[1])
+
+f = open("/tmp/test_fifo")
+
+while True:
+    data = f.read().rstrip()
+    if not data or not data.isdigit():
+        continue
+    m = colorModule.MailNotifier()
+    m.setColor(int(data))
