a48914a492cc9f9a7ea75c64918a4a690990386f
Alexander Lozovskoy <alozovskoy@gmail.com>
2014-03-12 10:30:31 +0400

    Добавил проверку на существование FIFO

--- colornotify.py
+++ colornotify.py
@@ -1,14 +1,21 @@
 import sys
 import os
 import imp
+import subprocess
+
 
 colorModule = imp.load_source('', sys.argv[1])
+fifoFileName = '/tmp/colornotify.fifo'
 
-f = open("/tmp/colornotify.fifo")
+if not os.path.exists(fifoFileName):
+    os.umask(000)
+    os.mkfifo(fifoFileName, 0666)
+
+fifo = open(fifoFileName)
 
 while True:
-    data = f.read().rstrip()
+    data = fifo.read().rstrip()
     if not data or not data.isdigit():
         continue
-    m = colorModule.MailNotifier()
-    m.setColor(int(data))
+    notifier = colorModule.MailNotifier()
+    notifier.setColor(int(data))
